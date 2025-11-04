46746aae7ff9947bca3c0018f838f8c3762939cd
Tim Yeung <tim.yeung@hku.hk>
2025-11-04 16:00:20 +0800

    Add .gitignore and remove dist from version control

--- dist/nodes/EssentialEa/EssentialEa.node.d.ts
+++
@@ -1,5 +0,0 @@
-import { type INodeType, type INodeTypeDescription, type IExecuteFunctions, type INodeExecutionData } from 'n8n-workflow';
-export declare class EssentialEa implements INodeType {
-    description: INodeTypeDescription;
-    execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]>;
-}
